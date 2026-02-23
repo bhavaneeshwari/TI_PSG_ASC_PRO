eeac144683d908902e80535d205028715e7ed56b
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-02-24 00:13:03 +0530

    Update uart.c

--- uart.c
+++ uart.c
@@ -15,20 +15,20 @@ void uart_getline(char *buffer, int max_len)
         /* Handle Enter key (Carriage Return or Newline) */
         if (c == '\r' || c == '\n') {
             buffer[count] = '\0'; // Null-terminate the string
-            //xil_printf("\r\n");   // Echo newline to terminal
+            xil_printf("\r\n");   // Echo newline to terminal
             break;
         } 
         /* Handle Backspace */
         else if (c == '\b' || c == 0x7F) {
             if (count > 0) {
                 count--;
-                //xil_printf("\b \b"); // Erase character on terminal screen
+                xil_printf("\b \b"); // Erase character on terminal screen
             }
         } 
         /* Handle regular characters */
         else if (count < max_len - 1) {
             buffer[count++] = c;
-            //xil_printf("%c", c);     // Echo character to terminal
+            xil_printf("%c", c);     // Echo character to terminal
         }
     }
 }
