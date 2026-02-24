51856c9281adb47a15672732cc48d7256da9adc5
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-02-24 19:02:30 +0530

    Update parser.c

--- new_reg_app/USER_LOGIC/parser.c
+++ new_reg_app/USER_LOGIC/parser.c
@@ -3,7 +3,6 @@
 #include "../axi_regs.h"
 #include "../corelogic/api.h"
 
-/* Custom hex parser to handle hex strings (e.g., "0x1A" or "1A") */
 static u32 parse_hex(const char *s)
 {
     u32 val = 0;
@@ -15,7 +14,6 @@ static u32 parse_hex(const char *s)
 
     while (*s)
     {
-        /* Shift applied safely only after confirming valid hex char */
         if (*s >= '0' && *s <= '9')
             val = (val << 4) | (*s - '0');
         else if (*s >= 'A' && *s <= 'F')
@@ -33,7 +31,7 @@ static u32 parse_hex(const char *s)
 
 void parse_and_store(char *cmd)
 {
-    /* Block new commands if currently busy */
+   
     if (READ8(REG_COMMAND) != 0) return;
 
     char *tok = strtok(cmd, " ");
@@ -63,7 +61,6 @@ void parse_and_store(char *cmd)
             WRITE32(REG_OPERAND(0), parse_hex(addr));
             u32 length = parse_hex(len);
             
-            /* Limit length to 6 to fit in Operands[2..7] */
             if (length > 6) length = 6; 
             WRITE32(REG_OPERAND(1), length);
             
@@ -84,10 +81,9 @@ void parse_and_store(char *cmd)
     }
     else 
     {
-        /* Safely handle invalid commands */
+
         WRITE8(REG_OPCODE, 0xFF); 
     }
 
-    /* Start execution */
     WRITE8(REG_COMMAND, 1);
 }
