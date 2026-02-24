66b98e19f5433f2c7f8616980435b691db7a368c
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-02-24 19:40:40 +0530

    Update uart.c

--- new_reg_app/USER_LOGIC/uart.c
+++ new_reg_app/USER_LOGIC/uart.c
@@ -16,25 +16,22 @@ void uart_getline(char *buffer, int max_len)
     char c;
     
     while (1) {
-        c = inbyte(); // Wait for character from terminal
+        c = inbyte(); 
         
-        /* Handle Enter key (Carriage Return or Newline) */
         if (c == '\r' || c == '\n') {
-            buffer[count] = '\0'; // Null-terminate the string
-            xil_printf("\r\n");   // Echo newline to terminal
+            buffer[count] = '\0'; 
+            xil_printf("\r\n");   
             break;
         } 
-        /* Handle Backspace */
         else if (c == '\b' || c == 0x7F) {
             if (count > 0) {
                 count--;
-                xil_printf("\b \b"); // Erase character on terminal screen
+                xil_printf("\b \b"); 
             }
         } 
-        /* Handle regular characters */
         else if (count < max_len - 1) {
             buffer[count++] = c;
-            xil_printf("%c", c);     // Echo character to terminal
+            xil_printf("%c", c);     
         }
     }
 }
