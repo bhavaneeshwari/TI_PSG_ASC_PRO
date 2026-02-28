4921d2822b874893f0dda3bcdb39c590dc5a05b9
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-03-01 05:26:04 +0530

    Update executor.c

--- new_reg_app/CORE_LOGIC/executor.c
+++ new_reg_app/CORE_LOGIC/executor.c
@@ -1,28 +1,19 @@
 #include "executor.h"
 #include "axi_regs.h"
 #include "api.h"
-#include "xil_printf.h"
 
-void executor_poll(void)
-{
-    if (READ8(REG_COMMAND) == 1)  
-    {
-        xil_printf("  [EXEC] REG_COMMAND = 1 (START detected)\r\n");
+void executor_poll(void) {
+    if (READ_CMD() == 1) {
+        WRITE_CMD(2); 
         
-        WRITE8(REG_COMMAND, 2);    /
-        xil_printf("  [EXEC] REG_COMMAND = 2 (Processing... BUSY)\r\n");
-
-        u8 opcode = READ8(REG_OPCODE);
+        u8 opcode = READ_OPCODE();
         u16 status = TI_AFE_RET_EXEC_FAIL; 
 
-        if (opcode < API_TABLE_SIZE && api_table[opcode])
-        {
-            status = api_table[opcode]();
+        if (opcode < API_TABLE_SIZE && api_table[opcode]) {
+            status = api_table[opcode](HW_OPERAND_BASE); 
         }
 
-        WRITE16(REG_STATUS, status);
-        
-        WRITE8(REG_COMMAND, 0);   
-        xil_printf("  [EXEC] REG_COMMAND = 0 (Execution DONE -> IDLE)\r\n");
+        WRITE_STATUS(status);
+        WRITE_CMD(0); 
     }
 }
