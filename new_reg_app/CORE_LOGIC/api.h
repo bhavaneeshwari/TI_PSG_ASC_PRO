f0986837152a6e52a7b2683bf1106841c9990fed
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-03-01 05:20:10 +0530

    Update api.h

--- new_reg_app/CORE_LOGIC/api.h
+++ new_reg_app/CORE_LOGIC/api.h
@@ -3,19 +3,19 @@
 
 #include "xil_types.h"
 
-#define TI_AFE_RET_EXEC_PASS 0x0000
-#define TI_AFE_RET_EXEC_FAIL 0x0001
-
-typedef u16 (*api_func_ptr)(void);
+typedef u16 (*api_func_ptr)(volatile u8 *operands);
 
 typedef enum {
-    OPCODE_WRITE = 0,
-    OPCODE_READ,
-    OPCODE_ARRAY_WRITE,
-    OPCODE_ARRAY_READ,
+    OPCODE_RAW_WRITE = 0,
+    OPCODE_RAW_READ,
+    OPCODE_BURST_WRITE,
+    OPCODE_BURST_READ,
+    OPCODE_RAW_WRITE_MULTI,
+    OPCODE_RAW_READ_MULTI,
+    OPCODE_BURST_WRITE_MULTI,
     API_TABLE_SIZE
 } opcode_t;
 
 extern api_func_ptr api_table[API_TABLE_SIZE];
 
-#endif /* API_H */
+#endif
