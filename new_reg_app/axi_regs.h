b8b2602f7ba8caab00e81d84f737415d01ff568c
Arvind_Prakash_T <arvindprakasht23@gmail.com>
2026-03-01 05:07:20 +0530

    Update axi_regs.h

--- new_reg_app/axi_regs.h
+++ new_reg_app/axi_regs.h
@@ -2,24 +2,23 @@
 #define AXI_REGS_H
 
 #include "xparameters.h"
-#include "xil_io.h"
 #include "xil_types.h"
-
+#include "xil_io.h"
 
 #define REG_BASE_ADDR  XPAR_REG_32IP_0_SREG_AXI_BASEADDR
-#define REG_OPERAND(i)   (0x00 + ((i) * 4))
-#define REG_OPCODE       0x20
-#define REG_COMMAND      0x21
-#define REG_STATUS       0x22
-#define REG_RESULT(i)    (0x24 + ((i) * 4))
 
+#define HW_OPERAND_BASE ((volatile u8 *)(REG_BASE_ADDR + 0x00)) 
+#define HW_RESULT_BASE  ((volatile u8 *)(REG_BASE_ADDR + 0x24)) 
 
-#define WRITE8(offset, value)  Xil_Out8(REG_BASE_ADDR + (offset), (u8)(value))
-#define WRITE16(offset, value) Xil_Out16(REG_BASE_ADDR + (offset), (u16)(value))
-#define WRITE32(offset, value) Xil_Out32(REG_BASE_ADDR + (offset), (u32)(value))
+#define REG_OPCODE      0x20 
+#define REG_COMMAND     0x21 
+#define REG_STATUS      0x22 
 
-#define READ8(offset)  Xil_In8(REG_BASE_ADDR + (offset))
-#define READ16(offset) Xil_In16(REG_BASE_ADDR + (offset))
-#define READ32(offset) Xil_In32(REG_BASE_ADDR + (offset))
+#define WRITE_OPCODE(val)  Xil_Out8(REG_BASE_ADDR + REG_OPCODE, (u8)(val))
+#define READ_OPCODE()      Xil_In8(REG_BASE_ADDR + REG_OPCODE)
+#define WRITE_CMD(val)     Xil_Out8(REG_BASE_ADDR + REG_COMMAND, (u8)(val))
+#define READ_CMD()         Xil_In8(REG_BASE_ADDR + REG_COMMAND)
+#define WRITE_STATUS(val)  Xil_Out16(REG_BASE_ADDR + REG_STATUS, (u16)(val))
+#define READ_STATUS()      Xil_In16(REG_BASE_ADDR + REG_STATUS)
 
-#endif 
+#endif
